1b2e0a9ebd73257933277207ba973d9ab2adf5a2
jas-raw <jasandinoa@gmail.com>
2021-03-16 12:25:48 -0500

    conversion de multiples archivo a la vez y cambio en codificacion

--- txt_json.py
+++ txt_json.py
@@ -1,7 +1,8 @@
 #fname = "D://Documentos/Gig/python/Canción-Rota.txt"
 import re
 import json
-import codecs
+import os
+import shutil
 
 def meta_extraction(line):
 
@@ -34,7 +35,7 @@ def song_extraction(line, tip):
 
     ''' Función de extracción de datos de la canción'''
 
-    line = line.strip()
+    line = line.rstrip()
     if (line != '' and line != None):
         if line.startswith('['):
             tip = 1
@@ -91,82 +92,84 @@ def guardar(js):
 
     ''' Función de guardado JSon'''
 
-    with open('{}-{}.json'.format(js["metadata"]['artista'],js["metadata"]['cancion']), 'w', newline='\n') as file:
+    with open('jsons/'+'{} - {}.json'.format(js["metadata"]['artista'],js["metadata"]['cancion']), 'w', newline='\n') as file:
         json.dump(js, file, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
 
-    fname = "Canción-Rota.txt"
-    fh = open(fname, encoding='utf8')
+    txts = os.listdir('txt/')
+    for tx in txts:
+        fname = "txt/"+tx
+        fh = open(fname, encoding='utf-8-sig')
 
-    js = dict()
-    metadata = dict()
-    cancion = list()
-    estrofa = dict()
-    tipo = ''
-    content = list()
-    verso = dict()
-    notas = ''
-    espacio = ''
-    letra = ''
+        js = dict()
+        metadata = dict()
+        cancion = list()
+        estrofa = dict()
+        tipo = ''
+        content = list()
+        verso = dict()
+        notas = ''
+        espacio = ''
+        letra = ''
 
-    ini = True
-    count = 1
-    spynotas = []
-    tipo_ant = ''
-    tip = 0
+        ini = True
+        count = 1
+        spynotas = []
+        tipo_ant = ''
+        tip = 0
 
 
-    for line in fh:
-        if count <= 8:
-            dato = meta_extraction(str(line))
-            if dato[0] != None:
-                metadata[dato[0]] = dato[1]
-                count += 1
-            if count == 8:
-                js['metadata'] = metadata
-        else:
-            dato = song_extraction(str(line), tip)
-            tip = dato[2]
-            dato.pop(2)
-            if dato[0] != None:
-                if dato[1] == 'tipo':
-                    tipo = dato[0]
-                    if tipo != tipo_ant and ini != True:
-                        estrofa = {
-                            'tipo' : tipo_ant,
-                            'contenido' : content
+        for line in fh:
+            if count <= 8:
+                dato = meta_extraction(str(line))
+                if dato[0] != None:
+                    metadata[dato[0]] = dato[1]
+                    count += 1
+                if count == 8:
+                    js['metadata'] = metadata
+            else:
+                dato = song_extraction(str(line), tip)
+                tip = dato[2]
+                dato.pop(2)
+                if dato[0] != None:
+                    if dato[1] == 'tipo':
+                        tipo = dato[0]
+                        if tipo != tipo_ant and ini != True:
+                            estrofa = {
+                                'tipo' : tipo_ant,
+                                'contenido' : content
+                            }
+                            cancion.append(estrofa)
+                            content = []
+                            verso = {}
+                        ini = False
+                        tipo_ant = tipo
+
+                    elif dato[1] == 'acorde':
+                        spynotas = def_sub_datos(dato)
+                        notas = spynotas[1]
+                        espacio = spynotas[0]
+                    elif dato[1] == 'letra':
+                        letras = def_sub_datos(dato)
+                        letra = letras[0]
+                        verso = {
+                            'notas' : notas,
+                            'espacio' : espacio,
+                            'letra' : letra
                         }
-                        cancion.append(estrofa)
-                        content = []
-                        verso = {}
-                    ini = False
-                    tipo_ant = tipo
-
-                elif dato[1] == 'acorde':
-                    spynotas = def_sub_datos(dato)
-                    notas = spynotas[1]
-                    espacio = spynotas[0]
-                elif dato[1] == 'letra':
-                    letras = def_sub_datos(dato)
-                    letra = letras[0]
-                    verso = {
-                        'notas' : notas,
-                        'espacio' : espacio,
-                        'letra' : letra
-                    }
-                    content.append(verso)
-                else:
-                    print('Ha sucedido un error de clasificación de la canción')
+                        content.append(verso)
+                    else:
+                        print('Ha sucedido un error de clasificación de la canción')
 
 
-    estrofa = {
-        'tipo' : tipo_ant,
-        'contenido' : content
-    }
-    cancion.append(estrofa)
-    js['canción'] = cancion
+        estrofa = {
+            'tipo' : tipo_ant,
+            'contenido' : content
+        }
+        cancion.append(estrofa)
+        js['canción'] = cancion
 
-    print(js)
-    guardar(js)
+        print(js)
+        guardar(js)
