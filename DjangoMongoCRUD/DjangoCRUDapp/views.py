6903716692d0f25eb0bc9be541cc1ad2fbb20706
CristianAMC <cristian701jt@gmail.com>
2021-03-31 15:47:41 -0500

    Creación de barra de busqueda para realizar querys

--- DjangoMongoCRUD/DjangoCRUDapp/views.py
+++ DjangoMongoCRUD/DjangoCRUDapp/views.py
@@ -3,3 +3,9 @@ from DjangoCRUDapp.models import Artista
 # Create your views here.
 
  
+def searchbar(request):
+    if request.method == 'GET':
+        query = request.GET.get('search')
+        search = Artista.objects.all().filter(artista=query)
+
+        return render(request, 'searchbar .html', {'post':search})
--- DjangoMongoCRUD/DjangoMongoCRUD/urls.py
+++ DjangoMongoCRUD/DjangoMongoCRUD/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from DjangoCRUDapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('searchbar/', views.searchbar, name='searchbar'),
 ]
