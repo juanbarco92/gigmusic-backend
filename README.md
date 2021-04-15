b540b0b3d368af812dd59f307da15a12fdf095de
jas-raw <jasandinoa@gmail.com>
2021-04-14 19:16:29 -0500

    definicion demas metodos http, aun incompatible con id, cambios en readme

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # gigmusic-backend
 
-## conversion de txt a json
+## conversion de txt a json e implementacion de api
 
 ## convierte archivos de texto con 8 metadatos, acorde, letra y tipo de estrofa en archivos json utf-8
+## Provee datos de canciones, desarrollado en python con fastapi
 
--- main.py
+++ main.py
@@ -7,14 +7,16 @@ import mongo
 
 ''' -------------------- Main -------------------- '''
 
+# uvicorn main:app --reload
+
 app = FastAPI()
 
-''' ---------- Root ---------- '''
+''' Root '''
 @app.get("/")
 async def read_root():
 	return {"GIGMUSIC": "FastAPI"}
 
-''' ---------- Metodos para artistas ---------- '''
+''' Metodos para artistas '''
 
 @app.get("/artist/{artist_id}")
 async def read_artist(nombre: str):
@@ -25,7 +27,15 @@ async def read_artist(nombre: str):
 async def update_artist(artist_id: int, artist: Artist):
 	return {"artist_nombre": artist.nombre, "artist_id": artist_id}
 
-''' ---------- Metodos para canciones ---------- '''
+@app.post("/artist/{artist_id}")
+async def create_artist(artist: Artist):
+	return {"artist_nombre": artist.nombre}
+
+@app.delete("/artist/{artist_id}")
+async def delete_artist(artist_id: int):
+	return {"artist_id": artist_id}
+
+''' Metodos para canciones '''
 
 @app.get("/song/{song_id}")
 async def read_song(cancion: str):
@@ -33,5 +43,13 @@ async def read_song(cancion: str):
 	return document
 
 @app.put("/song/{song_id}")
-async def update_song(song_id: int, song: Song):
+async def update_song(song: Song):
+	return {"song_nombre": song.nombre}
+
+@app.post("/song/{song_id}")
+async def create_song(song_id: int, song: Song):
 	return {"song_nombre": song.nombre, "song_id": song_id}
+
+@app.delete("/song/{song_id}")
+async def delete_song(song_id: int):
+	return {"song_id": song_id}
