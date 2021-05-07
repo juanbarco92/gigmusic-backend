2ffe94182ff0cab9be9e714bc24e2edff05305ca
jas-raw <jasandinoa@gmail.com>
2021-05-07 11:08:09 -0500

    busqueda mas inteligente, busqueda multicampo por palabra

--- gigmusic/artists/utils.py
+++ gigmusic/artists/utils.py
@@ -1,8 +1,22 @@
 from utils.utils import regexSearch
 
 def artist_filter(search):
-	regex_data = regexSearch(search)
-	return {'$or': [
-		{'nombre': regex_data}, 
-		{'genero': regex_data}
-	]}
+	if (search.find(' ') > 0) : 
+		busqueda = search.split(' ')
+		l = []
+		for buscar in busqueda:
+			regex_data = regexSearch(buscar.strip())
+			l.append(
+				{'$or': [
+						{'nombre': regex_data}, 
+						{'genero': regex_data}
+				]}
+			)
+		return {'$and': l}
+	else:
+		buscar = search.strip()
+		regex_data = regexSearch(buscar)
+		return {'$or': [
+					{'nombre': regex_data}, 
+					{'genero': regex_data}
+				]}
